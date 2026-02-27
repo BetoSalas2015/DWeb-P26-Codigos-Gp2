4c308ce86c926e92d69c95f63173c3abdbe4df4c
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2026-02-26 20:22:07 -0600

    Sesión 12 - 26/02/2026

--- 03-AngulaJS/js/app.js
+++ 03-AngulaJS/js/app.js
@@ -4,11 +4,34 @@ var app = angular.module('miApp', []);
 app.controller('miControlador', function($scope) {
     
     $scope.tareas = [
-        'Estudiar Angular',
-        'Hacer Ejercicio', 
-        'Leer Libro',
-        'Dormir 8 horas'
-    ];
+        { tarea: 'Estudiar Angular', prioridad: 2 },
+        { tarea: 'Hacer Ejercicio', prioridad: 1 },
+        { tarea: 'Leer Libro', prioridad: 3 },
+        { tarea: 'Dormir 8 horas', prioridad: 2 }
+    ]
+
+    $scope.nuevaTarea
+
     $scope.totalTareas = $scope.tareas.length;
     $scope.nuevaTarea = '';
+
+    $scope.agregarTarea = () => {
+        if($scope.nuevaTarea && $scope.nuevaPrioridad) {
+            $scope.tareas.push( {
+                tarea: $scope.nuevaTarea,
+                prioridad: $scope.nuevaPrioridad
+            } );
+            $scope.totalTareas = $scope.tareas.length;
+            $scope.nuevaTarea = '';
+            $scope.nuevaPrioridad = '';
+        } else {
+            alert('Por favor llnear los dos campos')
+        }
+    };
+
+    $scope.eliminarTarea = (index) => {
+        $scope.tareas.splice(index, 1);
+        $scope.totalTareas = $scope.tareas.length;
+    };
+
 });
